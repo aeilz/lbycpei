0453305dc002d4fca4fb2d65ab4a8c941e627a99
eliza <eliza_delarosa@dlsu.edu.ph>
2023-05-12 16:04:04 +0800

    Add files via upload

---
+++ IdeaProjects/newFile/src/HelloACM.java
@@ -0,0 +1,13 @@
+import acm.program.*;
+
+public class HelloACM extends ConsoleProgram {
+
+    public void run() {
+        println("Hello Eliza!!!!!");
+    }
+
+    public static void main(String[] args) {
+        (new HelloACM()).start(args);
+    }
+
+}
